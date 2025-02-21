8c7572a6c06ae373a82246a80d10ba69cb666d84
vilgen <emin.vilgenoglu@gmail.com>
2025-02-21 08:26:44 +0300

    product-service testcontainers mongodb upgrade

--- product-service/src/main/java/com/mapkloud/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/mapkloud/product_service/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.mapkloud.product_service.repository;
 
 import com.mapkloud.product_service.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
--- product-service/src/test/java/com/mapkloud/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/mapkloud/product_service/ProductServiceApplicationTests.java
@@ -3,21 +3,25 @@ package com.mapkloud.product_service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mapkloud.product_service.dto.ProductRequest;
 import com.mapkloud.product_service.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -25,19 +29,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Container
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.23");
-
 	@Autowired
 	private MockMvc mockMvc;
 	@Autowired
 	private ObjectMapper objectMapper;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@Container
+	static GenericContainer<?> mongoDBContainer = new GenericContainer<>("mongo:6.0")
+			.withExposedPorts(27017).withEnv("MONGO_INITDB_DATABASE", "product-service");
+
+	@BeforeAll
+	static void setup() {
+		mongoDBContainer.start();
+	}
 
 	@DynamicPropertySource
-	static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
-		dymDynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri",
+				() -> "mongodb://" + mongoDBContainer.getHost() + ":" + mongoDBContainer.getMappedPort(27017));
+	}
+
+	@Test
+	void testMongoDBConnection() {
+		assertNotNull(mongoTemplate);
 	}
 
 	@Test
